cea09471539138189df06847ebdcc23defd4a9d7
kangting <kangting@daojia.com>
2018-04-19 15:05:48 +0800

    定义模块和异步加载

--- js/app.js
+++ js/app.js
@@ -6,8 +6,14 @@ require.config({
     urlArgs: "bust=" + (new Date()).getTime() // 为方便开发调试加上时间戳避免缓存
 });
 require([
-    'jquery'
-], function($) {
+    'jquery',
+    './app/api'
+], function($, api) {
     //var str = helper.trim('   and    ');
     // console.log(str);
+    $('#user').on('click', function() {
+        api.getUser().then(function(user) {
+            console.log(user);
+        });
+    });
 });
---
+++ js/app/api.js
@@ -0,0 +1,13 @@
+define([
+    'jquery'
+], function($) {
+    return {
+        getUser: function() {
+            var def = $.Deferred();
+            require(['./app/user'], function(user) {
+                def.resolve(user);
+            });
+            return def;
+        }
+    }
+});
