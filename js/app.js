7c7e992a35e5f6f959b6e464c54738cc28f2dd6d
kangting <kangting@daojia.com>
2018-04-19 17:33:23 +0800

    map

--- js/app.js
+++ js/app.js
@@ -3,17 +3,24 @@ require.config({
     paths: {
         'jquery': 'lib/jquery-2.1.1.min'
     },
-    urlArgs: "bust=" + (new Date()).getTime() // 为方便开发调试加上时间戳避免缓存
+    urlArgs: "bust=" + (new Date()).getTime(), // 为方便开发调试加上时间戳避免缓存
+    map: {
+        'app/api': {
+            'jquery': 'lib/jquery-2.1.1.min'
+        },
+        'app/api2': {
+            'jquery': 'lib/jquery-2.1.2.min'
+        }
+    },
 });
 require([
-    'jquery',
-    './app/api'
-], function($, api) {
+    './app/api2'
+], function(api) {
     //var str = helper.trim('   and    ');
     // console.log(str);
-    $('#user').on('click', function() {
-        api.getUser().then(function(user) {
-            console.log(user);
-        });
-    });
+    // $('#user').on('click', function() {
+    //     api.getUser().then(function(user) {
+    //         console.log(user);
+    //     });
+    // });
 });
